97f7a3efad93b2cf6ac71582734f7d7d6a51aa2f
Prayas Gautam <prayas12gautam@gmail.com>
2023-05-22 15:26:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
-pip install pandas
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('pandas')
 import sys
 import json
-import pandas as pd
 import pickle
 model = pickle.load(open('model.pkl','rb'))
 data_dict = json.loads(sys.argv[1])
-df = pd.DataFrame.from_dict(data_dict, orient='index').T
+df = pandas.DataFrame.from_dict(data_dict, orient='index').T
 ans = model.predict(df)
 dum = 0
 ret = {}
